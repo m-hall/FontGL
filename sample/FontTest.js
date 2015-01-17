f310ff636f67c010833a4b12d351145019563575
Michael Hall <github@michaelhall.ca>
2015-01-17 17:37:45 -0500

    Update the Font Test for the Font updates

--- sample/FontTest.js
+++ sample/FontTest.js
@@ -3,26 +3,20 @@
 (function () {
     "use strict";
 
-    function log() {
-        console.log(this, arguments);
-    }
     function load() {
+        var openSans = new FontFace('open sans', 'url("http://fonts.gstatic.com/s/opensans/v10/DXI1ORHCpsQm3Vp6mXoaTaRDOzjiPcYnFooOUGCOsRk.woff") format("woff")', {});
         var f = new Font('monospace');
-        f.listen('resize', log);
-        f.listen('update', log);
         document.body.appendChild(f.canvas);
         f.addCharacters('~!@#$%^&*()_+{}|:"<>?`,./;\'[]\\-="Éá, é, í, ó, ú, ü, ñ ...…');
         f = new Font('arial');
         document.body.appendChild(f.canvas);
-        new FontFace('open sans', 'url("http://fonts.gstatic.com/s/opensans/v10/DXI1ORHCpsQm3Vp6mXoaTaRDOzjiPcYnFooOUGCOsRk.woff") format("woff")', {})
-            .load()
-            .then(function () {
-                f = new Font('open sans');
-                f.listen('resize', log);
-                f.listen('update', log);
-                document.body.appendChild(f.canvas);
-                f.addCharacters('~!@#$%^&*()_+{}|:"<>?`,./;\'[]\\-="Éá, é, í, ó, ú, ü, ñ ...…');
-            });
+
+
+        f = new Font(openSans);
+        openSans.loaded.then(function () {
+            document.body.appendChild(f.canvas);
+            f.addCharacters('~!@#$%^&*()_+{}|:"<>?`,./;\'[]\\-="Éá, é, í, ó, ú, ü, ñ ...…');
+        });
     }
 
     window.addEventListener('load', load, false);
