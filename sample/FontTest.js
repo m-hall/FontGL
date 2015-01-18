17995cc229b43ddc7d9b04836b506f8abf15d056
Michael Hall <github@michaelhall.ca>
2015-01-18 11:08:57 -0500

    Clean up the Font Test a little.

--- sample/FontTest.js
+++ sample/FontTest.js
@@ -1,11 +1,12 @@
 /*jslint browser: true */
-/*global Font */
+/*global Font, FontFace */
 (function () {
     "use strict";
+    var openSansSrc = 'url("http://fonts.gstatic.com/s/opensans/v10/DXI1ORHCpsQm3Vp6mXoaTaRDOzjiPcYnFooOUGCOsRk.woff") format("woff")';
 
     function load() {
-        var openSans = new FontFace('open sans', 'url("http://fonts.gstatic.com/s/opensans/v10/DXI1ORHCpsQm3Vp6mXoaTaRDOzjiPcYnFooOUGCOsRk.woff") format("woff")', {});
-        var f = new Font('monospace');
+        var openSans = new FontFace('open sans', openSansSrc, {}),
+            f = new Font('monospace');
         document.body.appendChild(f.canvas);
         f.addCharacters('~!@#$%^&*()_+{}|:"<>?`,./;\'[]\\-="Éá, é, í, ó, ú, ü, ñ ...…');
         f = new Font('arial');
